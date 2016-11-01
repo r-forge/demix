f32eb0eee91aa2f49eb3b45f77f166ff0f56e349
proebuck (P. Roebuck) <proebuck@mdanderson.org>
2016-11-01 23:26:11 +0000

    Added function argument prerequisite checks. Reformatted.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/demix@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeMix/R/Normalization.R
+++ pkg/DeMix/R/Normalization.R
@@ -8,26 +8,34 @@ DeMix.Normalization <- function(input,
                                 method=c("total", "quantile", "median"),
                                 groupid,
                                 ...) {
-  newt <- as.matrix(input)
-  
-  method <- match.arg(method)
+    ## Check arguments
+    stopifnot(is.matrix(input) || is.data.frame(input))
+    method <- match.arg(method)
+    stopifnot(is.numeric(groupid) && !anyNA(groupid))
 
-  colnames(newt) <- NULL
-  rownames(newt) <- NULL
-    
-  # 0 denotes normal cell / 1 denotes tumor cell
-  seqData <- newSeqCountSet(as.matrix(newt), groupid)
-  
-  # Quantile normalization
-   seqData <- estNormFactors(seqData, method)
-   k3 <- seqData@normalizationFactor
-   mk3 <- median(k3)
-   k3 <- k3/mk3
 
-  # Calculating normalization factor
-   for(i in 1:ncol(newt))
-     newt[,i] = newt[,i]/k3[i]
+    newt <- as.matrix(input)
+    stopifnot(is.matrix(newt) && is.numeric(newt[, 1]) && !anyNA(newt))
+    stopifnot(ncol(newt) == length(groupid))
 
-  newt
+    ## :PLR: Why throw away dimnames information?
+    colnames(newt) <- NULL
+    rownames(newt) <- NULL
+
+    ## 0 denotes normal cell / 1 denotes tumor cell
+    seqData <- DSS::newSeqCountSet(newt, groupid)
+
+    ## Quantile normalization
+    seqData <- DSS::estNormFactors(seqData, method)
+    k3 <- seqData@normalizationFactor
+    k3.median <- median(k3)
+    k3 <- k3 / k3.median
+
+    ## Calculating normalization factor
+    for (i in 1:ncol(newt)) {
+        newt[, i] <- newt[, i] / k3[i]
+    }
+
+    newt
 }
 
